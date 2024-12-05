319e5c6270030d09a7cfb998d9236b41e1f4c12f
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-12-05 20:34:40 +0200

    finished product microservice

--- HungerStation.Web/Models/ProductDto.cs
+++ HungerStation.Web/Models/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace HungerStation.Web.Models.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HungerStation.Web.Models.Dto;
 
 public class ProductDto
 {
@@ -8,4 +10,7 @@ public class ProductDto
     public string Description { get; set; }
     public string CategoryName { get; set; }
     public string ImageUrl { get; set; }
+    
+    [Range(1,100)]
+    public int Count { get; set; } = 1;
 }
