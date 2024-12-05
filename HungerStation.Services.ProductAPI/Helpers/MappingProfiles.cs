4078d839d202473c6a85a17799a08634db4333e6
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-12-05 20:16:09 +0200

    added product microservice and consuming it from the web project

---
+++ HungerStation.Services.ProductAPI/Helpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using HungerStation.Services.ProductAPI.Models;
+using HungerStation.Services.ProductAPI.Models.Dto;
+
+
+namespace HungerStation.Services.CouponAPI.Helpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Product, ProductDto>().ReverseMap();
+    }
+    
+  
+}
---
+++ HungerStation.Web/Service/IService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using HungerStation.Web.Models;
+using HungerStation.Web.Models.Dto;
+
+namespace HungerStation.Web.Service.IService;
+
+public interface IProductService
+{
+    Task<ResponseDto?> GetProductAsync(string couponCode);
+    Task<ResponseDto?> GetAllProductsAsync();
+    Task<ResponseDto?> GetProductByIdAsync(int id);
+    Task<ResponseDto?> CreateProductsAsync(ProductDto productDto);
+    Task<ResponseDto?> UpdateProductsAsync(ProductDto productDto);
+    Task<ResponseDto?> DeleteProductsAsync(int id);
+} 
